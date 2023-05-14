ddb2a8018f7eaa7847d876f44a00894162dd62b1
Dimkar3000 <Dimkar3000@gmail.com>
2023-05-14 23:11:48 +0300

    seperated coverage

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+fn return_hello() -> String {
+    "Hello, world!".to_string()
+}
+
+#[cfg(not(tarpaulin))]
 fn main() {
-    println!("Hello, world!");
+    println!("{}", return_hello());
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::return_hello;
+
+    #[test]
+    fn exploration() {
+        assert_eq!(2 + 2, 4);
+        return_hello();
+    }
 }
