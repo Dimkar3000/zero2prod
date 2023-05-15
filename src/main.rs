9329a3c9512425a3cb878c68e55cc51a6999adaa
Dimkar3000 <Dimkar3000@gmail.com>
2023-05-15 19:29:22 +0300

    added health check api route

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,23 @@
-fn return_hello() -> String {
-    "Hello, world!".to_string()
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
 }
 
-#[cfg(not(tarpaulin))]
-fn main() {
-    println!("{}", return_hello());
+async fn healt_check(_req: HttpRequest) -> impl Responder {
+    HttpResponse::Ok().finish()
 }
 
-#[cfg(test)]
-mod tests {
-    use crate::return_hello;
-
-    #[test]
-    fn exploration() {
-        assert_eq!(2 + 2, 4);
-        return_hello();
-    }
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(healt_check))
+            .route("/{name}", web::get().to(greet))
+            .route("/", web::get().to(greet))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
