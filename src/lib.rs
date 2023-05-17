d6a8b8b76647ee8233dbe13d575114596f9c5992
Dimkar3000 <Dimkar3000@gmail.com>
2023-05-17 21:47:29 +0300

    implemented subscription endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,17 @@
 use actix_web::{web, App, HttpServer};
 
-use sqlx::{Connection, PgConnection};
+use sqlx::PgPool;
 pub mod configuration;
 pub mod routes;
 
 use crate::routes::*;
 
 pub struct ApplicationState {
-    pub database_connection: PgConnection,
+    pub database_connection: PgPool,
 }
 
 impl ApplicationState {
-    pub fn new(database_connection: PgConnection) -> Self {
+    pub fn new(database_connection: PgPool) -> Self {
         ApplicationState {
             database_connection,
         }
@@ -22,7 +22,7 @@ pub async fn run() -> std::io::Result<()> {
     let config = configuration::get_configuration().expect("failed to get configuration");
 
     let address = config.get_url_to_bind();
-    let connection = PgConnection::connect(&config.database.get_connection_string())
+    let connection = PgPool::connect(&config.database.get_connection_string())
         .await
         .expect("failed to connect to the database.");
     let app_state = web::Data::new(ApplicationState::new(connection));
@@ -32,7 +32,7 @@ pub async fn run() -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
-            .route("/subscribe", web::get().to(subsribe))
+            .route("/subscribe", web::post().to(subsribe))
             .app_data(app_state.clone())
     })
     .bind(address)?
--- src/routes/subscribe.rs
+++ src/routes/subscribe.rs
@@ -1,12 +1,38 @@
 use actix_web::{web, HttpResponse};
 use serde::Deserialize;
+use sqlx::types::{chrono::Utc, Uuid};
+
+use crate::ApplicationState;
 
 #[derive(Deserialize)]
 pub struct SubscriptionFormDto {
-    pub email: String,
-    pub name: String,
+    email: String,
+    name: String,
 }
 
-pub async fn subsribe(_form: web::Form<SubscriptionFormDto>) -> HttpResponse {
-    HttpResponse::Ok().finish()
+pub async fn subsribe(
+    form: web::Form<SubscriptionFormDto>,
+    state: web::Data<ApplicationState>,
+) -> HttpResponse {
+    let state = state.get_ref();
+    let result = sqlx::query!(
+        r#"
+        INSERT INTO subscriptions(id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+    .execute(&state.database_connection)
+    .await;
+
+    match result {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(e) => {
+            println!("Failed to execute query: {}", e);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
 }
